f80a7acbd313138d8185d070d1991338a7513062
BaoBao-creator <ba0ba0113377@gmail.com>
2026-05-24 12:07:26 +0700

    Tải thư mục lên lần đầu

---
+++ src/client/java/b40/b40/client/B40Client.java
@@ -0,0 +1,10 @@
+package b40.b40.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class B40Client implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
