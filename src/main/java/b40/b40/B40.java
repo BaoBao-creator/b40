34c3948cc938e4634db52a20b61cc785829ed554
BaoBao-creator <ba0ba0113377@gmail.com>
2026-05-27 14:15:16 +0700

    Add admin item with right-click mode cycling

--- src/main/java/b40/b40/B40.java
+++ src/main/java/b40/b40/B40.java
@@ -1,10 +1,24 @@
 package b40.b40;
 
+import b40.b40.item.AdminWandItem;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 
 public class B40 implements ModInitializer {
+    public static final String MOD_ID = "b40";
+    public static final Item ADMIN_ITEM = Registry.register(
+            BuiltInRegistries.ITEM,
+            Identifier.fromNamespaceAndPath(MOD_ID, "admin"),
+            new AdminWandItem(new Item.Properties().stacksTo(1).rarity(Rarity.EPIC)));
+
     @Override
     public void onInitialize() {
-        // Mod logic removed.
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(entries -> entries.accept(ADMIN_ITEM));
     }
 }
